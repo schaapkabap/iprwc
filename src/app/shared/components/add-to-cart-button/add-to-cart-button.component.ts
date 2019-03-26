66cbdc15563d8e1b0fa0ef9e739991344b5af703
Alexander <alexiaan.vd@hotmail.com>
2019-03-26 10:47:28 +0100

    button html file to single line

--- src/app/shared/components/add-to-cart-button/add-to-cart-button.component.ts
+++ src/app/shared/components/add-to-cart-button/add-to-cart-button.component.ts
@@ -1,17 +1,19 @@
 import {Component, HostListener, Input, OnInit} from '@angular/core';
 import {Product} from '../../models/product.model';
 import {CartService} from '../../services/cart.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-to-cart-button',
-  templateUrl: './add-to-cart-button.component.html',
+  template: '<button class="card-link btn btn-info">Add to Cart</button>',
   styleUrls: ['./add-to-cart-button.component.sass']
 })
 export class AddToCartButtonComponent implements OnInit {
 
   @Input() product: Product;
 
-  constructor(private cart: CartService) {
+  constructor(private cart: CartService,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -20,6 +22,7 @@ export class AddToCartButtonComponent implements OnInit {
 
   @HostListener('click') onClick() {
     this.cart.addProduct(this.product);
+    this.router.navigate(['/app/cart']);
   }
 
 }
